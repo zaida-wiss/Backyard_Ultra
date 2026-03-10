a0102c306409b0eeeaa1144632a0a950f556f429
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-03-10 19:04:35 +0100

    rader och kolumner tillagt

--- backyard/src/components/Runners/Runners.tsx
+++ backyard/src/components/Runners/Runners.tsx
@@ -11,17 +11,35 @@ return (
 
   <table>
     <thead>
+      {/* Bygg tabellens rubrikrad */}
       <tr>
-        <th>ID</th>
+        {/* Kolumnrubriker */}
+        <th>Nr.</th>
         <th>Namn</th>
         <th>Medeltid</th>
+        {/* Skapar en ny rubrikkolumn för varje varv */}
         {Array.from({ length: maxLaps }).map((_,i) => (
-          <th key={i}>Varv{i+1}</th>
+          <th key={i}>{i+1}</th>
         ))}
       </tr>
     </thead>
+            <tbody>
+        {runners.map((runner) => (
+          <tr key={runner.id}>
+            <td>{runner.id}</td>
+            <td>{runner.name}</td>
+            <td>{useAverageLapTime(runner.lapTimes).toFixed(1)}</td>
+
+            {Array.from({ length: maxLaps }).map((_, i) => (
+              <td key={i}>
+                {runner.lapTimes[i] !== undefined ? runner.lapTimes[i] : "-"}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
   </table>
 
 );
-
+}
 export default Runners;
