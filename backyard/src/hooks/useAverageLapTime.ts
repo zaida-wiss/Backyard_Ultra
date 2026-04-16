991886e63f818fbc87fb9bd8c49dd588199ff450
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-04-16 20:04:37 +0200

    ??

--- backyard/src/hooks/useAverageLapTime.ts
+++
@@ -1,9 +0,0 @@
-import {useMemo} from "react";
-
-export function useAverageLapTime(lapTimes: number[]):number {
-  return useMemo(() => {
-  if (lapTimes.length === 0) return 0;
-  const total = lapTimes.reduce((sum, time) => sum + time, 0);
-  return total / lapTimes.length;
-}, [lapTimes]);
-}
